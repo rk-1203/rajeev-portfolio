1ed1f02296f9f87cae4be9bd67bf316ca9fc7595
rk-1203 <ritesh.kumar@rubrik.com>
2022-10-01 13:41:59 +0530

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/custom.global.scss'
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import "../styles/custom.global.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-M3J3KBX" });
+  }, []);
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
+export default MyApp;
