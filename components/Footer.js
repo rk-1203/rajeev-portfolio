5b41bd560981f1a980da0ef0f06de67ec39b62e0
rk-1203 <ritesh.kumar@rubrik.com>
2022-10-01 14:28:27 +0530

    content update

--- components/Footer.js
+++ components/Footer.js
@@ -34,7 +34,7 @@ export const Footer = () => {
     <footer className="bg-secondary text-center py-2 px-5">
       <div className="container text-muted">
         <small>
-          &copy; 2022. Made with love by{" "}
+          &copy; 2022. Made by{" "}
           <Link href="https://riteshkumar.netlify.app/">
             <a className="author">Ritesh Kumar</a>
           </Link>{" "}
